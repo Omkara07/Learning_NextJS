0806934aada1f4a5bfeeb02da4cb869e40be97f6
Omkara07 <singh.omkara07@gmail.com>
2024-11-06 00:40:22 +0530

    some fixes

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -8,36 +8,15 @@ type userType = {
 }
 const UserCard = () => {
     const [userData, setUserData] = useState<userType | null>(null)
-    const loadUserData = () => {
+    useEffect(() => {
         try {
             const userr = localStorage.getItem("user")
             if (userr) {
                 const u = JSON.parse(userr)
                 setUserData(u)
-            } else {
-                setUserData(null)
             }
-        } catch (error) {
-            console.log(error)
-        }
-    }
+        } catch (error) { console.log(error) }
 
-    useEffect(() => {
-        // Load data on initial render
-        loadUserData()
-
-        // Set up event listener for storage changes
-        const handleStorageChange = (event: StorageEvent) => {
-            if (event.key === 'user') {
-                loadUserData()
-            }
-        }
-        window.addEventListener('storage', handleStorageChange)
-
-        // Cleanup event listener on unmount
-        return () => {
-            window.removeEventListener('storage', handleStorageChange)
-        }
     }, [])
     return (
         <div className="p-8 rounded font-bold">

f97ff305bcae48d3a5fe3449e5c14ecdd15626b5
Omkara07 <singh.omkara07@gmail.com>
2024-11-05 23:59:48 +0530

    DB env

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import UserCard from '@/components/UserCard';
 
 export default async function Home() {
   return (
-    <div className="flex max-md:flex-col md:justify-around md:mt-20 gap-20 items-center text-2xl p-8 pb-20 max-md:gap-10 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex max-md:flex-col md:justify-around md:mt-20 gap-20 items-center text-2xl p-8 pb-20 max-md:gap-10 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
       <div className='md:w-1/2 justify-center md:px-32 md:-mt-40'>
         <UserCard />
       </div>

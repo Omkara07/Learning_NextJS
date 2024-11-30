c83eca4b5c0bb86b859daa2a4d472f3356d95f4c
Omkara07 <singh.omkara07@gmail.com>
2024-12-01 01:48:09 +0530

    nextconfig and package fixes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack(config, { isServer }) {
+    // Modify Webpack config here if necessary
+    return config;
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
